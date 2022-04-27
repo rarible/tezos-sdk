84a2cfe8b9925dc31f77a56a0903fe71cd068681
Florian PAUTOT <florian.pautot@gmail.com>
2022-04-27 17:21:57 +0200

    move back to testnet

--- packages/sdk/test/sale-v2-with-ft-fa12.ts
+++ packages/sdk/test/sale-v2-with-ft-fa12.ts
@@ -1,6 +1,6 @@
 import { AssetTypeV2 } from "@rarible/tezos-common";
 import {testScript} from "../main/script";
-import {awaitItem, awaitDevItem} from "./common/utils";
+import {awaitItem} from "./common/utils";
 
 async function sale() {
   const sellerEdsk = "edskRqrEPcFetuV7xDMMFXHLMPbsTawXZjH9yrEz4RBqH1D6H8CeZTTtjGA3ynjTqD8Sgmksi7p5g3u5KUEVqX2EWrRnq5Bymj"
@@ -8,22 +8,19 @@ async function sale() {
 
   const mintedItemId = await testScript('mint', {
     edsk: sellerEdsk,
-    contract: "KT1REVBNMiz2QuQjgGM9UsoPDmu7Jcm9gX6y",
-    is_dev: true,
-    amount: 1
+    contract: "KT1EreNsT2gXRvuTUrpx6Ju4WMug5xcEpr43"
   })
   console.log('mintedItemId', mintedItemId)
 
-  await awaitDevItem(mintedItemId)
+  await awaitItem(mintedItemId)
 
   //const mintedItemId = "KT1EreNsT2gXRvuTUrpx6Ju4WMug5xcEpr43:218"
 
   const sellOrder = await testScript('sell_v2_with_fa12', {
     edsk: sellerEdsk,
     item_id: mintedItemId,
-    ft_contract: "KT191NbZCWnCZvoLEgcPpSEx5yVRk7WnisnN",
+    ft_contract: "KT1WsXMAzcre2MNUjNkGtVQLpsTnNFhBJhLv",
     ft_token_id: undefined,
-    is_dev: true
   })
   console.log('sellOrder', sellOrder)
 
@@ -32,10 +29,9 @@ async function sale() {
     item_id: mintedItemId,
     owner: "tz1Mxsc66En4HsVHr6rppYZW82ZpLhpupToC",
     sale_type: AssetTypeV2.FA12,
-    ft_contract: "KT191NbZCWnCZvoLEgcPpSEx5yVRk7WnisnN",
+    ft_contract: "KT1WsXMAzcre2MNUjNkGtVQLpsTnNFhBJhLv",
     ft_token_id: undefined,
-    amount: 2,
-    is_dev: true
+    amount: 2
   })
   console.log('buyOrder', buyOrder)
 }
