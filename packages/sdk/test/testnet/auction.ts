567b83d271fdab99d9ef2fca153033fc5b16416b
Florian PAUTOT <florian.pautot@gmail.com>
2022-05-19 09:32:59 +0200

    Echo 117 improve tests (#12)
    fix: approve fa12 for fill order

---
+++ packages/sdk/test/testnet/auction.ts
@@ -0,0 +1,60 @@
+import {testScript} from "../../main/script";
+import {awaitItem} from "../common/utils";
+import {AssetTypeV2} from "@rarible/tezos-common";
+
+export async function auction() {
+  console.log("--------------------")
+  console.log("Running auction test")
+  console.log("--------------------")
+  const sellerEdsk = "edskRqrEPcFetuV7xDMMFXHLMPbsTawXZjH9yrEz4RBqH1D6H8CeZTTtjGA3ynjTqD8Sgmksi7p5g3u5KUEVqX2EWrRnq5Bymj"
+  const buyerEdsk = "edskS4QxJFDSkHaf6Ax3ByfrZj5cKvLUR813uqwE94baan31c1cPPTMvoAvUKbEv2xM9mvtwoLANNTBSdyZf3CCyN2re7qZyi3"
+
+  //const mintedItemId = "KT1EreNsT2gXRvuTUrpx6Ju4WMug5xcEpr43:253"
+
+  const mintedItemId = await testScript('mint', {
+    edsk: sellerEdsk,
+    contract: "KT1EreNsT2gXRvuTUrpx6Ju4WMug5xcEpr43"
+  })
+  console.log('mintedItemId', mintedItemId)
+
+  await awaitItem(mintedItemId)
+  const auctionOrder = await testScript('auction', {
+    edsk: sellerEdsk,
+    item_id: mintedItemId,
+    sale_type: AssetTypeV2.XTZ
+  })
+  console.log('auctionOrder', auctionOrder)
+
+  const cancelAuctionOrder = await testScript('cancel_auction', {
+    edsk: sellerEdsk,
+    item_id: mintedItemId
+  })
+  console.log('cancelAuctionOrder', cancelAuctionOrder)
+
+  const auctionOrder2 = await testScript('auction', {
+    edsk: sellerEdsk,
+    item_id: mintedItemId
+  })
+  console.log('auctionOrder2', auctionOrder2)
+
+  const bid = await testScript('put_auction_bid', {
+    edsk: buyerEdsk,
+    item_id: mintedItemId,
+    owner: "tz1Mxsc66En4HsVHr6rppYZW82ZpLhpupToC"
+  })
+  console.log('bid', bid)
+
+  console.log("Waiting for auction to finish...")
+  await delay(30000);
+
+  const finishAuctionOrder = await testScript('finish_auction', {
+    edsk: sellerEdsk,
+    item_id: mintedItemId,
+    owner: "tz1Mxsc66En4HsVHr6rppYZW82ZpLhpupToC"
+  })
+  console.log('finishAuctionOrder', finishAuctionOrder)
+}
+
+function delay(ms: number) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
+}
---
+++ packages/sdk/test/testnet/bundle_sale.ts
@@ -0,0 +1,48 @@
+import { AssetTypeV2 } from "@rarible/tezos-common";
+import {testScript} from "../../main/script";
+import {awaitItem} from "../common/utils";
+
+export async function bundle_sale() {
+  console.log("--------------------")
+  console.log("Running bundle_sale test")
+  console.log("--------------------")
+  const sellerEdsk = "edskRqrEPcFetuV7xDMMFXHLMPbsTawXZjH9yrEz4RBqH1D6H8CeZTTtjGA3ynjTqD8Sgmksi7p5g3u5KUEVqX2EWrRnq5Bymj"
+  const buyerEdsk = "edskS4QxJFDSkHaf6Ax3ByfrZj5cKvLUR813uqwE94baan31c1cPPTMvoAvUKbEv2xM9mvtwoLANNTBSdyZf3CCyN2re7qZyi3"
+
+  const mintedItemId_0 = await testScript('mint', {
+    edsk: sellerEdsk,
+    contract: "KT1EreNsT2gXRvuTUrpx6Ju4WMug5xcEpr43"
+  })
+  console.log('mintedItemId_0', mintedItemId_0)
+
+  await awaitItem(mintedItemId_0)
+
+  const mintedItemId_1 = await testScript('mint', {
+    edsk: sellerEdsk,
+    contract: "KT1EreNsT2gXRvuTUrpx6Ju4WMug5xcEpr43"
+  })
+  console.log('mintedItemId_1', mintedItemId_1)
+
+  await awaitItem(mintedItemId_1)
+
+  // const mintedItemId = ""
+  const sellOrder = await testScript('sell_bundle', {
+    edsk: sellerEdsk,
+    item_id: `${mintedItemId_0},${mintedItemId_1}`,
+    sale_type: AssetTypeV2.XTZ,
+    ft_contract: undefined,
+    ft_token_id: undefined,
+  })
+  console.log('sellOrder', sellOrder)
+
+  const buyOrder = await testScript('buy_bundle', {
+    edsk: buyerEdsk,
+    item_id: `${mintedItemId_0},${mintedItemId_1}`,
+    owner: "tz1Mxsc66En4HsVHr6rppYZW82ZpLhpupToC",
+    sale_type: AssetTypeV2.XTZ,
+    ft_contract: undefined,
+    ft_token_id: undefined,
+    amount: 0.02
+  })
+  console.log('buyOrder', buyOrder)
+}
