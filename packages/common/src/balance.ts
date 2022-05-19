067823b96787aeee452207daa3ecb9f5dc17b299
Aleksandr Sozinov <a.sozinov@rarible.com>
2022-05-19 17:20:54 +0300

    fix: get_balance

--- packages/common/src/balance.ts
+++ packages/common/src/balance.ts
@@ -8,28 +8,29 @@ export async function get_balance(
     asset_type: AssetTypeV2,
     asset_contract?: string,
     asset_token_id?: BigNumber
-    ) : Promise<string> {
-  let balance = "0"
-  if(asset_type == AssetTypeV2.XTZ){
-    const b_tz = await get_xtz_balance(config, owner)
-    balance = b_tz.toString()
-  } else if (asset_type == AssetTypeV2.FA2 || asset_type ==  AssetTypeV2.FA12){
-    if(asset_contract == undefined){
-      throw new Error("Contract can't be empty for FA12 and FA2 assets")
+    ) : Promise<BigNumber> {
+  switch (asset_type) {
+    case AssetTypeV2.XTZ: return get_xtz_balance(config, owner)
+    case AssetTypeV2.FA12:
+    case AssetTypeV2.FA2: {
+      if (asset_contract == undefined) {
+        throw new Error("Contract can't be empty for FA12 and FA2 assets")
+      }
+      const tokenIdQuery = asset_token_id !== undefined ? `&token.tokenId=${asset_token_id.toString()}` : ""
+      const r = await fetch(`${config.tzkt}/v1/tokens/balances?account=${owner}&token.contract=${asset_contract}${tokenIdQuery}`)
+      if (r.ok) {
+        const json = await r.json()
+        if (!json.length) {
+          return new BigNumber(0)
+        }
+        const factor = await asset_factor(config, asset_type, asset_contract, asset_token_id)
+        return new BigNumber(json[0].balance || 0).div(factor)
+      } else {
+        throw new Error(r.statusText)
+      }
     }
-    const tokenIdQuery = asset_token_id !== undefined ? `&token.tokenId=${asset_token_id.toString()}` : ""
-    const r = await fetch(`${config.tzkt}/v1/tokens/balances?account=${owner}&token.contract=${asset_contract}${tokenIdQuery}`)
-    if (r.ok) {
-      const json = await r.json()
-      const factor = await asset_factor(config, asset_type, asset_contract, asset_token_id)
-      balance = new BigNumber(json[0].balance).div(factor).toString()
-    } else {
-      throw new Error(r.statusText)
-    }
-  } else {
-    throw new Error("Unknown asset type")
+    default: throw new Error("Unknown asset type")
   }
-  return balance
 }
 
 export async function get_xtz_balance(config: Config, account: string) : Promise<BigNumber> {
