8df24bb10442b070dacc1a9b274fdad11714dd7b
Florian PAUTOT <florian.pautot@gmail.com>
2022-06-22 13:03:37 +0200

    fix url for union staging api

--- packages/sdk/main/script.ts
+++ packages/sdk/main/script.ts
@@ -141,7 +141,7 @@ export async function testScript(operation?: string, options: any = {}) {
     sig_checker: "KT1RGGtyEtGCYCoRmTVNoE6qg3ay2DZ1BmDs",
     tzkt: "https://api.ithacanet.tzkt.io",
     dipdup: "https://rarible-ithacanet.dipdup.net/v1/graphql",
-    union_api: "https://staging-api.rarible.org/v0.1"
+    union_api: "https://api-staging.rarible.org/v0.1"
   }
 
   const devConfig = {
