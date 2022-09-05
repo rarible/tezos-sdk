5454701a1c1be5d4d5e52f81b21731b9f0f73a5c
Florian PAUTOT <florian.pautot@gmail.com>
2022-09-05 11:27:03 +0200

    ECHO-257: fix cancel test

--- packages/sdk/test/testnet/rarible/cancel.ts
+++ packages/sdk/test/testnet/rarible/cancel.ts
@@ -9,28 +9,28 @@ export async function cancel() {
 
     const sellerEdsk = "edskRoV2U68QcqfPZ2g9PqxS23tSPH1kag61qiWQNH7GDWQjcfKr1vd9vdJm7MtoKp63kxvRWPJv2eyLMjgsJJPBx4SQVBjrgp"
 
-    // const mintedItemId = await testScript('mint', {
-    //     edsk: sellerEdsk,
-    //     contract: "KT1Uke8qc4YTfP41dGuoGC8UsgRyCtyvKPLA",
-    //     metadata: `{"": "ipfs://QmQ4x5BR7ecGVjyhZ7o87m2rPgzp8sBzxFbM4gtHiQQ6ay"}`,
-    // royalties: `{"tz1Mxsc66En4HsVHr6rppYZW82ZpLhpupToC": 1000}`,
-    //     amount: 100
-    // })
-    // console.log('mintedItemId', mintedItemId)
-    //
-    // await awaitItem(mintedItemId as string)
-    // // const mintedItemId = ""
-    // const sellOrder = await testScript('sell_v2', {
-    //     edsk: sellerEdsk,
-    //     item_id: mintedItemId,
-    //     qty: 1,
-    //     amount: 0.000002
-    // })
-    // console.log('sellOrder', sellOrder)
+    const mintedItemId = await testScript('mint', {
+        edsk: sellerEdsk,
+        contract: "KT1Uke8qc4YTfP41dGuoGC8UsgRyCtyvKPLA",
+        metadata: `{"": "ipfs://QmQ4x5BR7ecGVjyhZ7o87m2rPgzp8sBzxFbM4gtHiQQ6ay"}`,
+    royalties: `{"tz1Mxsc66En4HsVHr6rppYZW82ZpLhpupToC": 1000}`,
+        amount: 100
+    })
+    console.log('mintedItemId', mintedItemId)
+
+    await awaitItem(mintedItemId as string)
+    // const mintedItemId = ""
+    const sellOrder = await testScript('sell_v2', {
+        edsk: sellerEdsk,
+        item_id: mintedItemId,
+        qty: 1,
+        amount: 0.000002
+    })
+    console.log('sellOrder', sellOrder)
 
     const cancelOrder = await testScript('cancel', {
         edsk: sellerEdsk,
-        order_id: "a8179f5f-4bfb-55b8-b9ea-0bdb9003afbe"
+        order_id: sellOrder
     })
     console.log('cancelOrder', cancelOrder)
 
